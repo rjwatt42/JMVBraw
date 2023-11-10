0b70f698898623f7b66821c3c8e7e8fe628f3073
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2023-11-10 08:47:26 +0000

    more

--- BrawStats/R/brawsim.b.R
+++ BrawStats/R/brawsim.b.R
@@ -7,29 +7,26 @@ BrawSimClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
   private = list(
     .run = function() {
       
-      # set up global variables
+# set up global variables
       getGlobals()
       
-      # get some flags for later
+# get some flags for later
       makeSample<-self$options$makeValues
       doCopy<-self$options$copyValues
       doSend<-self$options$sendValues
       show<-self$options$show
-      
-      makeSample<-TRUE
 
-      doKeep<-TRUE
       doShow<-(show!="None")
+      doStore<-(doCopy || doSend)
       
-      if (!doShow) {
-        self$results$graphPlot$setVisible(FALSE)
-        self$results$graphPlot1$setVisible(FALSE)
-        self$results$graphPlot2$setVisible(FALSE)
-        self$results$graphPlot3$setVisible(FALSE)
-        self$results$reportPlot$setVisible(FALSE)
-      }
+      # get the stored data
+      dataStore<-self$results$tableStore$state
       
-      # make all the standard things we need
+      # debug information
+      # self$results$debug$setContent(TRUE)
+      # self$results$debug$setVisible(TRUE)
+      
+# make all the standard things we need
       locals<-getDefaults()
       
       # variables first
@@ -63,29 +60,111 @@ BrawSimClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
       locals$design$sOutliers<-self$options$Outliers
       
       locals$evidence$rInteractionOn<-self$options$doInteraction
-      
-      # get the stored data
-      dataStore<-self$results$tableStore$state
-      
-      # get the sample
+
       locals$sample<-dataStore$sample
       
-      # did we ask for a new sample?
+# did we ask for a new sample?
       if (makeSample) {
         # make a sample
         locals$sample<-makeSample(locals$IV,locals$IV2,locals$DV,locals$effect,locals$design)
+        # store it 
         dataStore$sample<-locals$sample
+        
+        # and list the first line of the data in the table
+        # we don't make this visible, except for testing
+        if (is.null(locals$IV2)) {
+          tableData<-cbind(participant=locals$sample$participant[1],iv=locals$sample$iv[1],iv2=0,dv=locals$sample$dv[1])
+        } else {
+          tableData<-cbind(participant=locals$sample$participant[1],iv=locals$sample$iv[1],iv2=locals$sample$iv2[1],dv=locals$sample$dv[1])
+        }
+        self$results$tableStore$setRow(rowNo=1,values=tableData)
       }
 
       # if we haven't got a sample, then do nothing more
-      # otherwise proceed and analyze the sample
-      if (length(locals$sample$iv)>0) {
-        # analyse the sample
-        locals$result<-analyseSample(locals$IV,locals$IV2,locals$DV,
-                              locals$effect,locals$design,locals$evidence,
-                              locals$sample)
+      if (!length(locals$sample$iv)>0) {
+        return()
+      }
+
+# prepare the variables for being stored 
+      if (doStore) {
+        #  convert Categorical values to strings
+        if (is.element(locals$IV$type,c("Categorical","Ordinal"))) {
+          locals$sample$iv<-as.character(locals$sample$iv)
+        }
+        if (!is.null(locals$IV2)) {
+          if (is.element(locals$IV2$type,c("Categorical","Ordinal"))) {
+            locals$sample$iv2<-as.character(locals$sample$iv2)
+          }
+        }
+        if (is.element(locals$DV$type,c("Categorical","Ordinal"))) {
+          locals$sample$dv<-as.character(locals$sample$dv)
+        }
         
-        # get the results
+        # get names for the new variables 
+        if (is.null(dataStore$iteration) || !self$options$appendValues)
+          iteration<-0
+        else   iteration<-dataStore$iteration+1
+        
+        if (iteration<1) suffix<-""
+        else             suffix<-paste0("_",iteration)
+        
+        # make the newVariables list
+        if (is.null(locals$IV2)) {
+          newVariables<-list(locals$sample$iv,locals$sample$dv)
+          names(newVariables)<-paste0(c(locals$IV$name,locals$DV$name),suffix)
+        } else {
+          newVariables<-list(locals$sample$iv,locals$sample$iv2,locals$sample$dv)
+          names(newVariables)<-paste0(c(locals$IV$name,locals$IV2$name,locals$DV$name),suffix)
+        }
+        
+        # merge with old variables (if appending)
+        if (is.null(dataStore$savedVariables) || !self$options$appendValues)
+          savedVariables<-newVariables
+        else {
+          oldNames<-names(dataStore$savedVariables)
+          savedVariables<-cbind(dataStore$savedVariables,newVariables)
+          names(savedVariables)<-c(oldNames,names(newVariables))
+        }
+        
+# are we copying to clipboard?      
+        if (doCopy) {
+          write_clip(savedVariables,allow_non_interactive = TRUE,col.names=FALSE)
+        }
+        
+# are we sending to Jamovi?      
+        if (doSend) {
+          nvars<-length(savedVariables)
+          keys<-1:nvars
+          measureTypes<-sapply(savedVariables,function(x) { if (is.character(x)) "nominal" else "continuous"})
+          
+          self$results$sendValues$set(keys=keys,names(savedVariables),
+                                      descriptions=rep("simulated",nvars),
+                                      measureTypes=measureTypes
+          )
+          for (i in keys) {
+            self$results$sendValues$setValues(index=i,savedVariables[[i]])
+          }
+        }
+      
+        # update the dataStore
+        dataStore$savedVariables<-savedVariables
+        dataStore$iteration<-iteration
+      }
+
+            
+# save everything for the next round      
+      self$results$tableStore$setState(dataStore)
+      self$results$tableStore$setVisible(FALSE)
+      
+      # are we showing the sample?
+      # if so, we'll need an analysis
+      if (doShow) {
+        # analyze the sample
+        locals$result<-analyseSample(locals$IV,locals$IV2,locals$DV,
+                                     locals$effect,locals$design,locals$evidence,
+                                     locals$sample)
+        
+        # prepare the output results
         switch(show,
                "Sample"={
                  outputText<-reportSample(locals$IV,locals$IV2,locals$DV,locals$design,locals$result)
@@ -100,14 +179,11 @@ BrawSimClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
                  outputGraph<-graphInference(locals$IV,locals$IV2,locals$DV,locals$effect,locals$design,locals$evidence,locals$result,"2D")
                }
         )
-        # self$results$debug$setContent(paste0("class graph = ",class(outputGraph)))
-        # self$results$debug$setVisible(TRUE)
         
-        if (doShow) {
         # main results graphs/reports
         self$results$reportPlot$setState(outputText)
         self$results$reportPlot$setVisible(TRUE)
-        
+
         if (length(outputGraph)==1) {
           self$results$graphPlot$setState(outputGraph[[1]])
           self$results$graphPlot$setVisible(TRUE)
@@ -132,98 +208,31 @@ BrawSimClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
           self$results$graphPlot3$setVisible(TRUE)
           self$results$graphPlot$setVisible(FALSE)
         }
-        }
-        
-        # list the first line of the data in the table as well
-        if (is.null(locals$IV2)) {
-          tableData<-cbind(participant=locals$sample$participant[1],iv=locals$sample$iv[1],iv2=0,dv=locals$sample$dv[1])
-        } else {
-          tableData<-cbind(participant=locals$sample$participant[1],iv=locals$sample$iv[1],iv2=locals$sample$iv2[1],dv=locals$sample$dv[1])
-        }
-        self$results$tableStore$setRow(rowNo=1,values=tableData)
-
-        if (doKeep) {
-          # prepare the variables for being stored 
-          #  convert Categorical values to strings
-          if (is.element(locals$IV$type,c("Categorical","Ordinal"))) {
-            locals$sample$iv<-as.character(locals$sample$iv)
-          }
-          if (!is.null(locals$IV2)) {
-            if (is.element(locals$IV2$type,c("Categorical","Ordinal"))) {
-              locals$sample$iv2<-as.character(locals$sample$iv2)
-            }
-          }
-          if (is.element(locals$DV$type,c("Categorical","Ordinal"))) {
-            locals$sample$dv<-as.character(locals$sample$dv)
-          }
-          
-          if (is.null(dataStore$iteration))  iteration<-0
-            else                             iteration<-dataStore$iteration
-          
-          if (self$options$appendValues) iteration<-iteration+1
-          if (iteration<=1) suffix<-""
-          else              suffix<-paste0("-",iteration)
-          
-          if (is.null(locals$IV2)) {
-            savedVariable<-list(locals$sample$iv,locals$sample$dv)
-            names(savedVariable)<-paste0(c(locals$IV$name,locals$DV$name),suffix)
-          } else {
-            savedVariable<-list(locals$sample$iv,locals$sample$iv2,locals$sample$dv)
-            names(savedVariable)<-paste0(c(locals$IV$name,locals$IV2$name,locals$DV$name),suffix)
-          }
-          
-          if (is.null(dataStore$savedVariables) || !self$options$appendValues)
-            savedVariables<-savedVariable
-          else
-            savedVariables<-cbind(dataStore$savedVariables,savedVariable)
-          
-          if (doCopy) {
-            write_clip(savedVariables,allow_non_interactive = TRUE,col.names=FALSE)
-          }
-          
-          if (doSend) {
-            nvars<-length(savedVariables)
-            keys<-1:nvars
-            measureTypes<-sapply(savedVariables,function(x) { if (is.character(x)) "nominal" else "continuous"})
-
-            self$results$sendValues$set(keys=keys,names(savedVariables),
-                                        descriptions=rep("simulated",nvars),
-                                        measureTypes=measureTypes
-            )
-            for (i in keys)
-              self$results$sendValues$setValues(index=i,savedVariables[[i]])
-
-          }
-          dataStore$savedVariables<-savedVariables
-          dataStore$iteration<-iteration
-        }
-      }
-      self$results$tableStore$setState(dataStore)
-      self$results$tableStore$setVisible(FALSE)
+      } # end of showing sample
+# end of actions      
     },
     
     .plotGraph=function(image, ...) {
       
       outputGraph <- image$state
-      if (is.null(outputGraph)) {
-        plot<-plotBlankTheme
+      if (!is.null(outputGraph)) {
+        print(outputGraph)
+        TRUE
       } else {
-        plot<-outputGraph
+        FALSE
       }
-      print(plot)
-      TRUE
     },
     
     .plotReport=function(image, ...) {
       
       outputText <- image$state
-      if (is.null(outputText)) {
-        plot<-plotBlankTheme
-      } else {
+      if (!is.null(outputText)) {
         plot<-reportPlot(outputText$outputText,outputText$nc,outputText$nr)        
+        print(plot)
+        TRUE
+      } else  {
+        FALSE
       }
-      print(plot)
-      TRUE
     }
   )
 )
